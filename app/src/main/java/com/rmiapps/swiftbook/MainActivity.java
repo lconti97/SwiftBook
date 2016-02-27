17e2566d09965b962823662c7345fd9a92fb1aad
lconti97 <lconti97@gmail.com>
2016-02-27 13:06:47 -0500

    Improve next button's behavior

--- app/src/main/java/com/rmiapps/swiftbook/MainActivity.java
+++ app/src/main/java/com/rmiapps/swiftbook/MainActivity.java
@@ -1,14 +1,13 @@
 package com.rmiapps.swiftbook;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity  extends Activity {
     EditText postText;
@@ -27,20 +26,43 @@ public class MainActivity  extends Activity {
         postText = (EditText) findViewById(R.id.post_text);
         nextButton = (Button) findViewById(R.id.next_button);
         postButton = (Button) findViewById(R.id.post_button);
+        likeButton = (Button) findViewById(R.id.like_button);
+        commentsButton = (Button) findViewById(R.id.comments_button);
         buttonBar = (LinearLayout) findViewById(R.id.button_bar);
+
+
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isPostView) {
-//                    buttonBar.removeView(postButton);
-                    LinearLayout.LayoutParams loparams =
-                            (LinearLayout.LayoutParams) postButton.getLayoutParams();
-                    loparams.weight = 0;
+                    hideView(postButton);
+                    postText.setEnabled(false);
+                    ViewGroup.LayoutParams loparams = postText.getLayoutParams();
                     loparams.width = 0;
-                    postButton.setLayoutParams(loparams);
+                    loparams.height = 0;
+                    postText.setLayoutParams(loparams);
+                    
+                    showButton(likeButton);
+                    showButton(commentsButton);
+                    isPostView = false;
                 }
-
             }
         });
+    }
+
+    private void hideView(View v) {
+        LayoutParams loparams = (LayoutParams) v.getLayoutParams();
+        loparams.weight = 0;
+        loparams.width = 0;
+        loparams.height = 0;
+        v.setLayoutParams(loparams);
+    }
+
+    private void showButton(Button b) {
+        LayoutParams loparams = (LayoutParams) b.getLayoutParams();
+        loparams.weight = 1;
+        loparams.width = LayoutParams.WRAP_CONTENT;
+        loparams.height = LayoutParams.WRAP_CONTENT;
+        b.setLayoutParams(loparams);
     }
 }
